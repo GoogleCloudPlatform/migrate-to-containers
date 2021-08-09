cbc8a546056223a8534487cb197e3570f90b7672
Eitan Eibschutz <eitane@google.com>
2021-08-09 13:02:37 +0000

    Change image type to UBUNTU
    Change-Id: I981528f3f193f6db902874adcee2af5e2054a2b3

--- examples/common/scripts/install_m4a.sh
+++ examples/common/scripts/install_m4a.sh
@@ -26,10 +26,11 @@ gcloud iam service-accounts keys create m4a-install.json --iam-account=m4a-insta
 
 # create a GKE processing cluster
 gcloud container clusters create m4a-proc-cluster \
- --project ${PROJECT_ID} --zone=${ZONE_ID} --num-nodes 1 --machine-type "n1-standard-4" --image-type "UBUNTU_CONTAINERD" --enable-stackdriver-kubernetes
+ --project ${PROJECT_ID} --zone=${ZONE_ID} --num-nodes 1 --machine-type "n1-standard-4" --image-type "UBUNTU" --enable-stackdriver-kubernetes
 
 # connect to GKE cluster
 gcloud container clusters get-credentials m4a-proc-cluster --zone ${ZONE_ID} --project ${PROJECT_ID}
 
 # run migctl install
 migctl setup install --json-key=m4a-install.json
+
