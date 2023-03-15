4903462846a7199f8b4adb938dfd7f0bfa12d059
Yair Halberstadt <yairhalberstadt@gmail.com>
2023-03-15 16:23:02 +0200

    Create mfit_collect_aws_guest_ssm.sh

---
+++ scripts/mFit/mfit_collect_aws_guest_ssm.sh
@@ -0,0 +1,81 @@
+version=$(curl -s https://mfit-release.storage.googleapis.com/latest)
+
+linux_collect_script=$(curl "https://mfit-release.storage.googleapis.com/$version/mfit-linux-collect.sh")
+# Create a script which runs the linux collect script and outputs the resulting archive to STDOUT.
+linux_script="
+set -e;
+script='${linux_collect_script//\'/\'\"\'\"\'}';
+sudo -n bash -c \"\$script\" collect-script --output mfit-collect.tar;
+encoded_tar=\$(base64 -w0 mfit-collect.tar);
+echo '<START_ARCHIVE>';
+echo \"\$encoded_tar\";
+echo '<END_ARCHIVE>';
+"
+# Convert the script into a valid json string.
+linux_json_escaped=$(echo "$linux_script" | jq -Rsa .)
+
+
+windows_collect_script=$(curl "https://mfit-release.storage.googleapis.com/$version/mfit-windows-collect.ps1")
+# Create a script which runs the windows collect script and outputs the resulting archive to STDOUT.
+windows_script="
+\$ErrorActionPreference = 'Stop'
+
+Invoke-Command {
+$windows_collect_script
+} -ArgumentList ('mfit-collect', '--minimal')
+
+try { \$archive = Get-Content -path 'mfit-collect.zip' -Encoding byte }
+catch {
+	try { \$archive = Get-Content -path 'mfit-collect.tar.gz' -Encoding byte }
+	catch { \$archive = Get-Content -path 'mfit-collect.tar' -Encoding byte }
+}
+
+\$encoded = [convert]::ToBase64String(\$archive)
+
+Write-Output '<START_ARCHIVE>'
+Write-Output \$encoded
+Write-Output '<END_ARCHIVE>'
+"
+# Escape the script according to https://msdn.microsoft.com/en-us/library/ms880421.
+# This is needed due to a bug in ssm agent for windows.
+windows_escaped_windows_script="\"$(echo "$windows_script" | sed 's/\(\\*\)\"/\1\1\\\"/g')
+\"";
+# Convert the script into a valid json string.
+windows_json_escaped=$(echo "$windows_escaped_windows_script" | jq -Rsa .)
+
+# For all AWS instances:
+aws ssm describe-instance-information "$@" | jq -c '.InstanceInformationList | .[]' | while read instance; do
+  id=$(echo $instance | jq -r '.InstanceId')
+  os=$(echo $instance | jq -r '.PlatformType')
+  if [ "$os" == "Linux" ]; then
+    continue
+    json_escaped=$linux_json_escaped;
+  elif [ "$os" == "Windows" ]; then
+    json_escaped=$windows_json_escaped;
+  else
+    echo "Instance $id has unsupported platform type $os" >&2;
+    continue;
+  fi
+
+  echo "Collecting $os VM $id"
+
+  # Run collection via ssm manager.
+  result=$(unbuffer aws ssm start-session --target $id --document AWS-StartInteractiveCommand --parameters "{ \"command\": [$json_escaped]}");
+  # Strip ansii escape codes from STDOUT.
+  result=$(echo "$result" | sed -r 's/\x1B\[(([0-9]+)(;[0-9]+)*)?[m,K,H,f,J]//g')
+  # Find the encoded archive between the tags <START_ARCHIVE> and <END_ARCHIVE> by stripping everything before and after those tags.
+  encoded=${result#*<START_ARCHIVE>}
+  encoded=${encoded%<END_ARCHIVE>*}
+
+  # Check we managed to find the tags - if nothing was stripped, then they weren't found.
+  if [ "$encoded" == "$result" ]; then
+    echo "Could not extract base64 encoded tar for $id from stdout \"$(echo "$result" | sed -z 's/\n/\\n/g')\"" >&2;
+    continue;
+  fi
+
+  # Decode the archive, write to a temp file, and import to mfit.
+  tmpfile=$(mktemp)
+  echo "$encoded" | base64 -di > $tmpfile
+  mfit discover import $tmpfile
+  rm $tmpfile
+done
