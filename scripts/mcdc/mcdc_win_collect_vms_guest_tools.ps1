cb1840f8e591356a3a7378c9f3c45b119f1227c4
Eitan Eibschutz <eitane@google.com>
2023-11-27 23:21:30 +0000

    add scripts for using mcdc CLI on Windows
    GitOrigin-RevId: 62955948f6e17437ad14b0ef32fbbdbd7c2dfa7b

---
+++ scripts/mcdc/mcdc_win_collect_vms_guest_tools.ps1
@@ -0,0 +1,64 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script reads the input CSV_FILE and iterates over all VMs, running guest
+# collection on all VMs via VMWare guest tools and authenticating to the VMs using
+# username and password. The default USERNAME and PASSWORD arguments will be used
+# unless either of them is overriden for a specific VM.
+
+$url_regex = "^https?://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$"
+
+$CSV_FILE = Read-Host -Prompt "CSV file name:"
+if (-not (Test-Path $CSV_FILE)) {
+    Write-Host "The file $CSV_FILE does not exist or can not be accessed."
+    exit 1
+}
+
+$VSPHERE_URL = Read-Host -Prompt "vSphere URL:"
+if (-not ($VSPHERE_URL -match $url_regex)) {
+    Write-Host "vSphere URL $VSPHERE_URL IS not a valid URL"
+    exit 1
+}
+
+$vsphere_creds = Get-Credential -Message "Please enter your VMWare username and password."
+$vsphere_creds.GetNetworkCredential().Username
+
+$vm_creds = Get-Credential -Message "Please enter your default username and password to login to the VMs."
+$vm_creds.GetNetworkCredential().Username
+
+# CSV fields
+#NAME;PLATFORM VM ID;OS;IP;USERNAME;PASSWORD
+$FileContents = Get-Content -Path $CSV_FILE
+
+ForEach ($Line in $FileContents) {
+    $vm_name, $vm_id, $os, $ip, $username, $password = $Line.Split(";")
+
+    if (-not $username) {
+        $username = $vm_creds.GetNetworkCredential().Username
+    }
+    Write-Host "Password: $password"
+    if (-not $password) {
+        $password = $vm_creds.GetNetworkCredential().Password
+    }
+
+    if (-not $vm_id) {
+        Write-Host "Skipping VM: $vm_name because it has no VM ID"
+        continue
+    }
+    
+    # Note that the below only works for VMWare VMs
+    # vm_id after the last slash if prefixed by vSphere IP
+    $mod_vm_id = Split-Path -Path $vm_id -Leaf
+    ./mcdc discover vsphere guest --url $VSPHERE_URL -u $vsphere_creds.GetNetworkCredential().Username -p $vsphere_creds.GetNetworkCredential().Password --vm-user $username --vm-password $password $mod_vm_id --proxy socks5://localhost:38004 -i
+}
---
+++ scripts/mcdc/mcdc_win_discovery_export_guest_tools.ps1
@@ -0,0 +1,32 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script lists all the VMs that were discoverd by mcdc and outputs to
+# a file named vms.csv.
+# The CSV format fields are: Name;Platform VM ID;OS;
+# The USERNAME and PASSWORD fields will NOT be set by the script and can be
+# modified by the user if unique credentials are required for a VM.
+
+$tmp_vms = (./mcdc report --format csv) | select -skip 2
+
+Out-File -FilePath .\vms.csv
+# CSV fields
+#NAME;PLATFORM VM ID;OS;IP;USERNAME;PASSWORD
+ForEach ($Line in $tmp_vms) {
+    $internal_id, $vm_name, $vm_id, $os_family, $tmp = $Line.Split(",")
+ 
+    $vm = "$vm_name;$vm_id;$os_family;;;"
+   
+    $vm | Out-File -FilePath .\vms.csv -Append   
+}
